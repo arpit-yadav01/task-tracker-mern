d8ec80f8f80d2a100ed747d84cd62b4ef2a39a8a
arpit-yadav01 <arpityadav170402@gmail.com>
2026-01-03 01:56:25 +0530

    Wrap App with BrowserRouter to fix routing error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,9 +8,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <>
+    <BrowserRouter>
       <App />
       <ToastContainer position="top-right" autoClose={3000} />
-    </>
+    </BrowserRouter>
   </React.StrictMode>
 );
