6936bdafbddbe008e262d332f99428b86ec60b58
arpit-yadav01 <arpityadav170402@gmail.com>
2026-01-02 23:22:12 +0530

    Hardcode backend API URL to fix production issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// HARD GUARANTEED BASE URL
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    "https://task-tracker-mern-9fla.onrender.com/api"
+  baseURL: "https://task-tracker-mern-9fla.onrender.com/api"
 });
 
 export default api;
